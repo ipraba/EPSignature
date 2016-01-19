2d382d3556b85ba81e531ff76e368b2f1dddb18c
Prabaharan <mailprabaharan.e@gmail.com>
2016-01-19 18:10:16 +0530

    Updated project for Travis CI support ✅

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,1 +1,23 @@
-// https://github.com/Quick/Quick
+import XCTest
+
+import EPSignature
+
+class EPCalendarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test1 () {
+        let signatureView = EPSignatureView(frame: CGRectMake(0,0,240,320))
+        XCTAssertNil(signatureView.getSignatureAsImage())
+        XCTAssertFalse(signatureView.isSigned)
+    }
+    
+}
